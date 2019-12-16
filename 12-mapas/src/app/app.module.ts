ad8934dc0bb47847daf69add407029e388c4ee1a
Cuesta Gutierrez <BICUGUAL@intranet.ibermatica>
2019-12-16 08:14:33 +0100

    proyecto 12

---
+++ 12-mapas/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './../app/material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { MapaEditarComponent } from './components/mapa/mapa-editar.component';
+
+import { AgmCoreModule } from '@agm/core';
+
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  entryComponents: [
+    MapaEditarComponent
+  ],
+  declarations: [
+    AppComponent,
+    MapaComponent,
+    MapaEditarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDko4R427ou1cXsrk6cd4E0_-gdcYUQOuY'
+    })
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
